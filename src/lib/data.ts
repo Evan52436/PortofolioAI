f2b66eefcca58090ffac05cbbfb6be792ad41760
Evan Pranawa Armansyah <evan.pranawaa@gmail.com>
2025-12-12 06:52:04 +0000

    update title

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -8,7 +8,7 @@ const getImage = (id: string): ImagePlaceholder => {
 
 export const studentData = {
     name: 'Evan Pranawa Armansyah',
-    major: 'Rekayasa Perangkat Lunak',
+    major: 'Rekayasa Perangkat Lunak & Cybersecurity',
     bio: 'A tech enthusiast based in Bogor, Indonesia. Currently still in school but I\'ve worked on various personal projects including home class servers and network mapping that have helped me develop a strong foundation in Cybersecurity. I also have experience in music industry such as performing on stage, playing with DAW and many more',
     profilePicture: getImage('profile'),
 };
