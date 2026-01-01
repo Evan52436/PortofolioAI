420eec8039ca570f69d8f175dbab70c44d7eb89f
boiled potato <unboiledpotato@gmail.com>
2026-01-01 01:37:29 +0000

    add react snowfall to the to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import Snowfall from 'react-snowfall';
 import { Header } from '@/components/layout/header';
 import { Footer } from '@/components/layout/footer';
 import { Hero } from '@/components/sections/hero';
@@ -19,6 +20,7 @@ export default function Home() {
 
   return (
     <div className="flex min-h-screen flex-col">
+      <Snowfall />
       <Header />
       <main className="flex-1">
         <Hero />
